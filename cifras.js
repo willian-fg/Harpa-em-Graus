937f91c848674e52f5dab16732f9a112e507794b
Willian-fg <willflorgasp@gmail.com>
2025-06-21 07:25:05 -0300

    plena_paz iniciada

--- cifras.js
+++ cifras.js
@@ -1,5 +1,7 @@
 const cifras = [
   { nome: "1 - Chuvas de Graça", arquivo: "chuvas.html" },
   { nome: "2 - Saudosa Lembrança", arquivo: "saudosa.html" },
+  { nome: "3 - Plena Paz", arquivo: "plena.html" },
+  
   // Adicione outras cifras aqui
 ];
