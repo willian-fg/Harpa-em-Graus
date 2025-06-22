94973de5608486e20f30fd0cb06dd20cf2d3d670
Willian-fg <willflorgasp@gmail.com>
2025-06-22 07:55:43 -0300

    Nomes dos arquivos cifras alterados

--- cifras.js
+++ cifras.js
@@ -1,7 +1,7 @@
 const cifras = [
-  { nome: "1 - Chuvas de Graça", arquivo: "chuvas.html" },
-  { nome: "2 - Saudosa Lembrança", arquivo: "saudosa.html" },
-  { nome: "3 - Plena Paz", arquivo: "plena.html" },
+  { nome: "1 - Chuvas de Graça", arquivo: "1chuvas.html" },
+  { nome: "2 - Saudosa Lembrança", arquivo: "2saudosa.html" },
+  { nome: "3 - Plena Paz", arquivo: "3plena.html" },
   
   // Adicione outras cifras aqui
 ];
