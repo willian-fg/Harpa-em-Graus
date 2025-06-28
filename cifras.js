a4e648064bb5ab0391654a8722027fea1bc91358
Willian-fg <willflorgasp@gmail.com>
2025-06-28 17:37:24 -0300

    Nomes alterados, 15 terminado.

--- cifras.js
+++ cifras.js
@@ -1,7 +1,8 @@
 const cifras = [
-  { nome: "1 - Chuvas de Graça", arquivo: "1chuvas.html" },
-  { nome: "2 - Saudosa Lembrança", arquivo: "2saudosa.html" },
-  { nome: "3 - Plena Paz", arquivo: "3plena.html" },
+  { nome: "1 - Chuvas de Graça", arquivo: "1-chuvas.html" },
+  { nome: "2 - Saudosa Lembrança", arquivo: "2-saudosa.html" },
+  { nome: "3 - Plena Paz", arquivo: "3-plena.html" },
+  { nome: "15 - Conversão", arquivo: "15-conversão.html"}
   
   // Adicione outras cifras aqui
 ];
