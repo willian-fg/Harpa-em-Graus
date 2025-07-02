2efeef5cb5122e44e1bd41e3831873f07f06676a
Willian-fg <willflorgasp@gmail.com>
2025-07-01 22:56:12 -0300

    8

--- cifras.js
+++ cifras.js
@@ -2,6 +2,7 @@ const cifras = [
   { nome: "1 - Chuvas de Graça", arquivo: "1-chuvas.html" },
   { nome: "2 - Saudosa Lembrança", arquivo: "2-saudosa.html" },
   { nome: "3 - Plena Paz", arquivo: "3-plena.html" },
+  { nome: "8 - Cristo, O Fiel Amigo", arquivo: "8-ofielamigo.html"},
   { nome: "15 - Conversão", arquivo: "15-conversão.html"},
   { nome: "36 - O Exilado", arquivo: "36-oexilado.html"},
   { nome: "39 - Alvo Mais Que A Neve", arquivo: "39-alvo.html"},
