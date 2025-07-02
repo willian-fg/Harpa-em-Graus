a7dc081026234f373311ea961804b4484d25f62b
Willian-fg <willflorgasp@gmail.com>
2025-07-01 22:34:00 -0300

    36

--- cifras.js
+++ cifras.js
@@ -3,6 +3,7 @@ const cifras = [
   { nome: "2 - Saudosa Lembrança", arquivo: "2-saudosa.html" },
   { nome: "3 - Plena Paz", arquivo: "3-plena.html" },
   { nome: "15 - Conversão", arquivo: "15-conversão.html"},
+  { nome: "36 - O Exilado", arquivo: "36-oexilado.html"},
   { nome: "39 - Alvo Mais Que A Neve", arquivo: "39-alvo.html"},
   { nome: "115 - Trabalhai E Orai", arquivo: "115-trabalhai.html"},
   { nome: "212 - Os Guerreiros Se Preparam", arquivo: "212-osguerreiros.html"}
